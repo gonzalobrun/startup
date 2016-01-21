d5ea013a69639a30558d059a7d7694f3ee45686e
gonzalobrun <gonzalobrun11@gmail.com>
2016-01-21 13:42:02 -0300

    Creation index.html with stylesheets, jQuery and the required items, step 1 - 6

---
+++ 01-js-intro-and-jquery/jquery/myjs.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(".hide").fadeIn(1500, function() {
+  	  $(".alias").focus();
+	});
+});
+
+$(document).ready(function() {
+    $(".content").fadeIn(1000);
+});
+
+$(document).ready(function() {
+    $(".button").fadeIn(1000);
+});
+
+$(document).ready(function() {
+    $(".header").slideDown(1000);
+});
+
