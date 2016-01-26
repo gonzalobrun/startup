dbedef0e74d66f171edb5b0c7ade75ec2118d237
gonzalobrun <gonzalobrun11@gmail.com>
2016-01-26 08:57:44 -0300

    Function added to show the message and error in index.html

--- 01-js-intro-and-jquery/jquery/myjs.js
+++ 01-js-intro-and-jquery/jquery/myjs.js
@@ -1,25 +1,19 @@
 $(document).ready(function() {
+	$(".header").slideDown(1000);
     $(".hide").fadeIn(1500, function() {
   	  $(".alias").focus();
 	});
 });
 
 $(document).ready(function() {
-    $(".content").fadeIn(1000);
+    $(".content, .button").fadeIn(1000);
 });
 
-$(document).ready(function() {
-    $(".button").fadeIn(1000);
+$(document).ready(function(){
+  $(".button").click(function() {
+    $.get("http://bootcamp.dsupport2000.com/Natalia").always(function(resp, textStatus, jqxhr) {
+        $(".response").append(textStatus === "success" ? resp.response.greeting + ", " + resp.response.Welcome : textStatus + ", " + jqxhr);
+        });    
+    });
 });
 
-$(document).ready(function() {
-    $(".header").slideDown(1000);
-});
-
- $(document).ready(function(){
- 	$('.button').click(function(){
- 		var $resp = $.get("http://bootcamp.dsupport2000.com/Natalia", function(resp){
- 			$('.response').append(resp.response.greeting + ", " + resp.response.Welcome);
- 		});
- 	});
- });
