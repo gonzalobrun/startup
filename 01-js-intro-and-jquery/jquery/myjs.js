72b6fa506b305b347921ead50623fd436808e4f6
gonzalobrun <gonzalobrun11@gmail.com>
2016-01-27 15:04:17 -0300

    AJAX call fail

--- 01-js-intro-and-jquery/jquery/myjs.js
+++ 01-js-intro-and-jquery/jquery/myjs.js
@@ -24,3 +24,25 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $(".search").click(function(){
+        alert("THIS IS A TEST");
+        $.ajax({
+            url:'https://api.spotify.com/v1/search',
+            data: {"q":'Rolling Stones', "type":'album'},
+            type: 'GET',
+            dataType: 'json',
+            done: function  (json){
+                $.each(json), function(index, value){
+                    $.each(value.item), function(index, item)
+                    {
+                        $("article.spot").append("Name: "+ item.name+ "<br>");
+                        $("article.spot").append("Type: " + item.type + "<br>");
+                        $("article.spot").append("Album Cover: <img src='" + item.images[1].url + "'/><br>");
+                        $("article.spot").append("<a href='" + item.external_urls.spotify + "'>Play It!</href>");
+                    }
+                }
+            }
+        });
+    });
+});
