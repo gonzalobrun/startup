2549bb3cf5c7aac2f833a1b9e7cafaaf6adcbf7f
gonzalobrun <gonzalobrun11@gmail.com>
2016-02-02 10:06:25 -0300

    Observer working and displaying the changes on the HTML

--- 02-oop-inheritance/js/script.js
+++ 02-oop-inheritance/js/script.js
@@ -13,6 +13,7 @@ Movie.prototype.play = function(){
 	//console.log(this.name + " is playing now");
 	observer1.play();
 	return observer1.history();
+	//alert(observer1.history());
 };
 
 Movie.prototype.stop = function(){
@@ -46,8 +47,7 @@ Movie.prototype.get = function(attr, value){
 // 2- Instantiate some of your favorite movies and play with them in the console.
 
 var movie1 = new Movie("Man of Steel", "Action", 138, "23/06/2013");
-var movie2 = new Movie("The Avengers", "Action", 132, "23/04/2012");
-var movie3 = new Movie("Oblivion", "Action", 123, "12/04/2013");
+var movie2 = new Movie("Oblivion", "Action", 123, "12/04/2013");
 
 // 3- Add a MovieObserver class that listens for "playing" and “stopped” events.
 // I added the MovieObserver.status property to the Play and Stop methods.
@@ -70,24 +70,28 @@ MovieObserver.prototype.stop = function(){
 }
 
 MovieObserver.prototype.history = function(){
+	var output = "History: <br/>";
 	for(i = 0; i < this.observer.length; i++){
 		console.log(this.observer[i]);
-		//return this.observer[i];
+		output += this.observer[i] + '<br />';
 	}
+	return output;
 }
 
 var observer1 = new MovieObserver();
 
 $(document).ready(function(){
 	$("#btn1").click(function(){
-	//	$("#history1").empty();
+		$("#history1").empty();
 		$("#history1").append(movie1.play());
 	});
 });
 
 $(document).ready(function(){
 	$("#btn2").click(function(){
-	//	$("#history1").empty();
+		$("#history1").empty();
 		$("#history1").append(movie1.stop());
 	});
 });
+
+
